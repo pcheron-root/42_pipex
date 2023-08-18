cacea3b0c7cdc07d65aec7a9e717bbbf7f63fbff
pcheron-pathways <pablo.cheron@proton.me>
2023-08-18 13:00:03 +0200

    a pipex to rule them all

--- src/dup.c
+++ src/dup.c
@@ -6,7 +6,7 @@
 /*   By: pcheron <pcheron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 21:04:29 by pcheron           #+#    #+#             */
-/*   Updated: 2023/08/18 12:48:11 by pcheron          ###   ########.fr       */
+/*   Updated: 2023/08/18 12:53:42 by pcheron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,14 @@ bool	ft_dup_bonus(int i, int fd[2], char **argv, t_env *env)
 	}
 	else if (argv[i + 4 + env->hd_status]) // si je suis une commande du milieu
 	{
-		if (dup2(env->pipeline[i % 2][1], 1) < 0)
+		if (dup2(env->pipeline[(i + 1) % 2][0], 0) < 0)
 			return (write(2, "pipex : dup2 failed\n", 21), false);
-		if (dup2(env->pipeline[(i + 1) % 2], 0) < 0)
+		if (dup2(env->pipeline[i % 2][1], 1) < 0)
 			return (write(2, "pipex : dup2 failed\n", 21), false);
 	}
 	else	// si je suis la derniere commande
 	{
-		if (dup2(env->pipeline[(i + 1) % 2], 0) < 0)
+		if (dup2(env->pipeline[(i + 1) % 2][0], 0) < 0)
 			return (write(2, "pipex : dup2 failed\n", 21), false);
 		if (dup2(fd[1], 1) < 0)
 			return (write(2, "pipex : dup2 failed\n", 21), false);
